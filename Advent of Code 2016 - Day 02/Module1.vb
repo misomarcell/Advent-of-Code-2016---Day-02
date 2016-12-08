e19843d09f42e67f4e44cfad9f17317a2d065c86
Misó Marcell <misomarcell@gmail.com>
2016-12-08 16:42:54 +0100

    I hope it works

--- Advent of Code 2016 - Day 02/Module1.vb
+++ Advent of Code 2016 - Day 02/Module1.vb
@@ -3,22 +3,31 @@
 
     Dim x As Integer = 2
     Dim y As Integer = 2
+    Dim pad As New DataTable
 
     Sub Main()
 
+        pad.Columns.Add("First")
+        pad.Columns.Add("Second")
+        pad.Columns.Add("Third")
+        pad.Rows.Add("1", "2", "3")
+        pad.Rows.Add("4", "5", "6")
+        pad.Rows.Add("7", "8", "9")
+
+        Dim solution As String = ""
         Dim input_list As New ArrayList
         input_list = readFile()
         For i As Integer = 0 To input_list.Count - 1
             Dim input_line As String = input_list(i)
             For c As Integer = 0 To input_line.Length - 1
-                Dim chr As String = input_line(c)
                 'D - U - R - L
-                doStep(chr)
-                Console.Write(chr)
+                doStep(input_line(c))
+                Console.Write(input_line(c))
             Next
-            Console.WriteLine(x & "; " & y)
+            Console.WriteLine("> POSITION: " & x & " ; " & y)
+            solution = solution & pad.Rows(y - 1).Item(x - 1)
         Next
-
+        Console.WriteLine("> SOLUTION: " & solution)
         Dim abc As String = Console.ReadLine
     End Sub
 
